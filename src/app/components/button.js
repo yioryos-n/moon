30bf8129e028b95f70e3e4728ce0dd51010d76cf
Georgios Nizamis <grgsnizamis@gmail.com>
2025-05-27 19:20:10 +0300

    rebuilt (again)

--- src/app/components/button.js
+++ src/app/components/button.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export function ReturnButton() {
     return (
-        <Link href="/">
+        <Link href="/moon/">
           <button type="button" className="text-lg text-shadow-small hover:text-shadow-hover-dark hover:duration-500 hover:cursor-pointer">Back to Homepage</button>
         </Link>
     )
