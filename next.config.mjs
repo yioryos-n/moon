e02c932e6c963ad9db8dc9fd64fb7459101414b5
Georgios Nizamis <grgsnizamis@gmail.com>
2025-05-27 16:25:33 +0300

    fix next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
   output: 'export',
   basePath: '/moon', // e.g. '/moon'
   trailingSlash: true, // recommended for GitHub Pages
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
